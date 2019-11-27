06526a7c0fdf84122ae6067ff7a583759d473d95
Luis Otávio <luismtns@gmail.com>
2019-11-27 00:50:28 -0300

    Plugin Firebase adicionado e configurado

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -39,6 +39,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.Keyboard"
       ]
+    },
+    {
+      "id": "cordova-plugin-firebase.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebase/www/firebase.js",
+      "pluginId": "cordova-plugin-firebase",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -47,6 +55,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-device": "2.0.2",
     "cordova-plugin-splashscreen": "5.0.2",
     "cordova-plugin-ionic-webview": "4.1.3",
-    "cordova-plugin-ionic-keyboard": "2.2.0"
+    "cordova-plugin-ionic-keyboard": "2.2.0",
+    "cordova-plugin-firebase": "2.0.5"
   };
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChNvbLJi-mZgS_XkopwPP502NGHrxEack",
+    authDomain: "gardener-1563f.firebaseapp.com",
+    databaseURL: "https://gardener-1563f.firebaseio.com",
+    projectId: "gardener-1563f",
+    storageBucket: "gardener-1563f.appspot.com",
+    messagingSenderId: "178808083741",
+    appId: "1:178808083741:web:468d3b37eef52047f9bdf4",
+    measurementId: "G-MNF5X0MB1S"
+  }
 };
 
 /*
